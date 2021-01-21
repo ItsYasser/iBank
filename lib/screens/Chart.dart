43dace764da1035cc1ee122726876e013b2f7b25
Yasser Harzelli <67924433+El-Mysto@users.noreply.github.com>
2021-01-21 01:49:31 +0100

    Update Chart.dart

--- lib/screens/Chart.dart
+++ lib/screens/Chart.dart
@@ -11,57 +11,66 @@ class Chart extends StatelessWidget {
           height: 200,
           width: double.infinity,
           // color: Colors.red,
-          child: AspectRatio(
-            aspectRatio: 1.3,
-            child: Row(
-              children: [
-                Expanded(
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: PieChart(
-                      PieChartData(
-                          borderData: FlBorderData(show: false),
-                          centerSpaceRadius: double.infinity,
-                          sectionsSpace: 0,
-                          // read about it in the below section
-                          sections: [
-                            PieChartSectionData(
-                              value: 25,
-                              color: Colors.amber,
-                              showTitle: false,
-                            ),
-                            PieChartSectionData(
-                              value: 25,
-                              showTitle: false,
-                              color: Colors.blue,
-                            ),
-                            PieChartSectionData(
-                              value: 25,
-                              showTitle: false,
-                              color: Colors.green,
-                            ),
-                            PieChartSectionData(
-                              value: 25,
-                              showTitle: false,
-                              color: Colors.deepPurple,
-                            ),
-                          ]),
-                    ),
-                  ),
+          child: Row(
+            children: [
+              Expanded(
+                child: PieChart(
+                  PieChartData(
+                      borderData: FlBorderData(show: false),
+                      centerSpaceRadius: double.infinity,
+                      sectionsSpace: 0,
+                      // read about it in the below section
+                      sections: [
+                        PieChartSectionData(
+                          value: 25,
+                          color: Colors.amber,
+                          showTitle: false,
+                        ),
+                        PieChartSectionData(
+                          value: 25,
+                          showTitle: false,
+                          color: Colors.blue,
+                        ),
+                        PieChartSectionData(
+                          value: 25,
+                          showTitle: false,
+                          color: Colors.green,
+                        ),
+                        PieChartSectionData(
+                          value: 25,
+                          showTitle: false,
+                          color: Colors.deepPurple,
+                        ),
+                      ]),
                 ),
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Indicator(
-                      text: "Entertainement",
-                      color: Colors.blue,
-                      isSquare: false,
-                    )
-                  ],
-                )
-              ],
-            ),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Indicator(
+                    text: "Entertainement",
+                    color: Colors.blue,
+                    percentage: 25,
+                  ),
+                  Indicator(
+                    text: "Entertainement",
+                    color: Colors.green,
+                    percentage: 25,
+                  ),
+                  Indicator(
+                    text: "Entertainement",
+                    color: Colors.deepPurple,
+                    percentage: 25,
+                  ),
+                  Indicator(
+                    text: "Entertainement",
+                    color: Colors.amber,
+                    percentage: 25,
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
@@ -72,40 +81,53 @@ class Chart extends StatelessWidget {
 class Indicator extends StatelessWidget {
   final Color color;
   final String text;
-  final bool isSquare;
-  final double size;
   final Color textColor;
-
+  final double percentage;
   const Indicator({
     Key key,
     this.color,
     this.text,
-    this.isSquare,
-    this.size = 16,
+    this.percentage,
     this.textColor = const Color(0xff505050),
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            shape: isSquare ? BoxShape.rectangle : BoxShape.circle,
-            color: color,
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: color,
+            radius: 9,
+            child: Container(
+              height: 10,
+              width: 10,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+            ),
           ),
-        ),
-        const SizedBox(
-          width: 4,
-        ),
-        Text(
-          text,
-          style: TextStyle(
-              fontSize: 16, fontWeight: FontWeight.bold, color: textColor),
-        )
-      ],
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            text,
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.bold, color: textColor),
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            "${percentage.round()} %",
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.bold, color: textColor),
+          ),
+        ],
+      ),
     );
   }
 }
